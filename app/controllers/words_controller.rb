7cf05db9bd905b14c04c41abcaef3c68713b28a4
shotaro yamamoto <shotoron@gmail.com>
2020-07-22 17:50:08 +0900

    一覧画面を追加

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,13 +1,18 @@
 class WordsController < ApplicationController
+  def index
+    @words = Word.all
+  end
+
   def new
     @word = Word.new
   end
 
   def create
     @word = Word.new(word_params)
+    @word.save
   end
-  private
 
+  private
   def word_params
     params.require(:word).permit(:content)
   end
